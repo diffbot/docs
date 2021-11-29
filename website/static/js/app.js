ee4bcea7d960d92fa6bcb4777ebaf5a1ba2398aa
Merrill <merrill@diffbot.com>
2021-11-29 15:42:22 -0600

    add GTM script

--- website/static/js/app.js
+++ website/static/js/app.js
@@ -4,6 +4,12 @@ function resizeIframe(obj) {
   obj.style.height =
     obj.contentWindow.document.documentElement.scrollHeight + "px";
 }
+//Add google tag manager
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N42WS7K');
 
 (function (funcName, baseObj) {
   // The public function name defaults to window.docReady
@@ -190,6 +196,8 @@ docReady(function () {
         docMainWrapper.innerHTML;
     }
   }
+
+ 
 
   // Add fathom analytics
   let script = document.createElement("script");
