a6df5199d8083dc9a290934f7b1ed1a28ac2a425
Bruno <swader@users.noreply.github.com>
2021-11-30 00:29:29 +0000

    Deploy website
    Deploy website version based on f3f2f52bdb0c9fdf4f1df21ff0d05bf6c896a1ca

--- js/app.js
+++ js/app.js
@@ -4,12 +4,15 @@ function resizeIframe(obj) {
   obj.style.height =
     obj.contentWindow.document.documentElement.scrollHeight + "px";
 }
-//Add google tag manager
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N42WS7K');
+//Add google analytics
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-74291704-1');
+
+
 
 (function (funcName, baseObj) {
   // The public function name defaults to window.docReady
@@ -197,7 +200,14 @@ docReady(function () {
     }
   }
 
- 
+ //google analytics external script
+  let google_analytics = document.createElement("script");
+  google_analytics.setAttribute("src","https://www.googletagmanager.com/gtag/js?id=UA-74291704-1")
+  document.querySelector("body").appendChild(google_analytics);
+
+
+
+
 
   // Add fathom analytics
   let script = document.createElement("script");
